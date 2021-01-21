8be04b43b12cf64a379159509f75b14c9ed68514
Steven Song <stevensong077@gmail.com>
2021-01-22 10:15:49 +1100

    change url

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
-export const url = "http://localhost:4000";
+// export const url = "http://localhost:4000";
+export const url = "http://13.54.108.236/:4000";
 
 export const mapKey = "AIzaSyCXXn7SJ9ecHFrWFJQxZJ9988gTl7koixs";
