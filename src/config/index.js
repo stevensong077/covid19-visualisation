647a423b6e8ee116de2d2d38e7a67678262f0a29
Steven Song <stevensong077@gmail.com>
2021-01-28 11:54:46 +1100

    change remote server ip

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,8 @@
 // export const url = "http://localhost:4000";
-export const url = "http://13.54.108.236:4000";
+export const url = "http://13.54.182.63:4000";
+
+// "http://covid19monitorvic.tk:4000"
+
+// "https://covid19monitorvic.tk/api"
 
 export const mapKey = "AIzaSyCXXn7SJ9ecHFrWFJQxZJ9988gTl7koixs";
