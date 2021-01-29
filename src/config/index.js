5db2a69f18f3e1bd50e8269b7b3d880415380ba0
Steven Song <stevensong077@gmail.com>
2021-01-29 18:07:43 +1100

    change https to http in url

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,7 @@
 // export const url = "http://localhost:4000";
-export const url = "https://13.54.182.63:4000";
+export const url = "http://13.54.182.63:4000";
 
 // "http://covid19monitorvic.tk:4000"
-
 // "https://covid19monitorvic.tk/api"
 
 export const mapKey = "AIzaSyCXXn7SJ9ecHFrWFJQxZJ9988gTl7koixs";
