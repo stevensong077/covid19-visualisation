4f02de38d1807ef3bcc5dbc3ad9a1c85d34e2291
Steven Song <stevensong077@gmail.com>
2021-02-03 16:16:12 +1100

    change url variable form http to https

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 // export const url = "http://localhost:4000";
 // export const url = "http://13.54.182.63:4000";
 
-export const url = "http://covid19monitorvic.tk/api"
+export const url = "https://covid19monitorvic.tk/api"
     // "https://covid19monitorvic.tk/api"
 
 export const mapKey = "AIzaSyCXXn7SJ9ecHFrWFJQxZJ9988gTl7koixs";
